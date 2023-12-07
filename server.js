dd2f3c5697233cc05673100505b6616133d16fe3
= <=>
2023-12-07 11:45:08 +0800

    Added winston logger

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const routes = require('./src/routes')
+const { httpLogger, logger } = require('./lib/logger')
 
 // Get Chuck Norris joke categories
+app.use(httpLogger)
 app.use('/api', routes)
 
+app.use((err, req, res, next) => {
+  const statusCode = err.status || 500;
+  const errorMessage = err.message || 'Internal Server Error';
+
+  res.status(statusCode).json({ error: errorMessage, stack: err?.stack });
+});
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
